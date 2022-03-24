0de67d1b0d3de98763fd327ddc0458da9656d49d
StormShadonw <luisjorgelopezhernandez@hotmail.com>
2022-03-24 19:37:59 -0400

    Game Finished

--- Models/player.py
+++ Models/player.py
@@ -1,7 +1,35 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.start()
+        self.setheading(90)
+
+    def up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def left(self):
+        if self.xcor() > -280:
+            self.goto(self.xcor() - MOVE_DISTANCE, self.ycor())
+
+    def right(self):
+        if self.xcor() < 280:
+            self.goto(self.xcor() + MOVE_DISTANCE, self.ycor())
+
+    def finished(self):
+        if self.ycor() >= FINISH_LINE_Y:
+            return True
+        else:
+            return False
+
+    def start(self):
+        self.goto(STARTING_POSITION)
--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 import time
 from turtle import Screen
-from player import Player
-from car_manager import CarManager
-from scoreboard import Scoreboard
+from Models.player import Player
+from Models.car_manager import CarManager
+from Models.scoreboard import Scoreboard
+# from car_manager import CarManager
+# from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager()
+scoreBoard = Scoreboard()
+
+screen.listen()
+
+screen.onkey(player.up, "Up")
+screen.onkey(player.left, "Left")
+screen.onkey(player.right, "Right")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreBoard.game_over()
+
+        if player.finished():
+            player.start()
+            car_manager.level_up()
+            scoreBoard.increase_level()
+
+screen.exitonclick()
